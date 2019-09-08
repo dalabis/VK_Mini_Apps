5975a42bead1fa9488cab3257db126d2dbebfdc7
dalabis <si17t@mail.ru>
2019-09-08 15:34:02 +0300

    Updates

---
+++ precache-manifest.e2f2e9eb5c065793b2f0c19e15cc181b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "59b2088b0ab87fb82ff1",
+    "url": "/static/js/main.2a65d9d7.chunk.js"
+  },
+  {
+    "revision": "ed23b31c58be40b06612",
+    "url": "/static/js/2.57b14748.chunk.js"
+  },
+  {
+    "revision": "59b2088b0ab87fb82ff1",
+    "url": "/static/css/main.ab384e69.chunk.css"
+  },
+  {
+    "revision": "ed23b31c58be40b06612",
+    "url": "/static/css/2.697ded91.chunk.css"
+  },
+  {
+    "revision": "baf6fd7348b01c2eb97ce94e085934a1",
+    "url": "/index.html"
+  }
+];
